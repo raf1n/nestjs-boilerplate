ac329f345fb816c4f6928d9403713486e1ec6ca2
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-03-05 13:57:34 +0600

    mongo-added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { ConfigModule } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [UsersModule, ConfigModule.forRoot(), MongooseModule.forRoot('mongodb://localhost/users')],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      "mongodb+srv://rafin:CHoOPq3tTfzNx1DM@cluster0.khgm0kt.mongodb.net/?retryWrites=true&w=majority/serverDeployTest/users"
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
