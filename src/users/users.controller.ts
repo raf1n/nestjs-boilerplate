0f772115cafa8a61d990f46f595ebabcae13e119
Rahim Uddin Rafin <rahim.rafin.1@gmail.com>
2023-03-05 14:20:19 +0600

    k

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { LoginUserDto } from './dto/login-user.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { LoginUserDto } from "./dto/login-user.dto";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
-  constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
-  @Post('/login')
+  @Post("/login")
   login(@Body() loginUserDto: LoginUserDto) {
     return this.usersService.login(loginUserDto);
   }
 
-  @Post('/test_login')
-  test_login(@Body() loginUserDto: LoginUserDto) {
-    return "Test login" 
+  @Post("/test_login")
+  register(
+    @Body()
+    loginUserDto: {
+      firstName: string;
+      lastName: string;
+      email: string;
+    }
+  ) {
+    return this.usersService.register(loginUserDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,39 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { LoginUserDto } from './dto/login-user.dto';
-import { User, UserDocument } from './schemas/user.schema';
-import { JwtService } from '@nestjs/jwt';
-import { TokenVerifier } from 'src/utils/TokenVerifier';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { LoginUserDto } from "./dto/login-user.dto";
+import { User, UserDocument } from "./schemas/user.schema";
+import { JwtService } from "@nestjs/jwt";
+import { TokenVerifier } from "src/utils/TokenVerifier";
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService
+  ) {}
 
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>, private jwtService: JwtService) { }
+  async register(registerUserDto: {
+    firstName: string;
+    lastName: string;
+    email: string;
+  }) {
+    const result = await this.userModel.create({
+      firstName: registerUserDto.firstName,
+      lastName: registerUserDto.lastName,
+      email: registerUserDto.email,
+    });
+    return result;
+  }
 
   async login(loginUserDto: LoginUserDto) {
     const { token, tokenType } = loginUserDto;
-    var isVerified = false
-    var accessToken = null
+    var isVerified = false;
+    var accessToken = null;
 
-    if (tokenType == 'facebook') {
-      isVerified = await TokenVerifier.verifyFacebookToken(token)
-    }
-    else if (tokenType == 'google') {
+    if (tokenType == "facebook") {
+      isVerified = await TokenVerifier.verifyFacebookToken(token);
+    } else if (tokenType == "google") {
       isVerified = await TokenVerifier.verifyGoogleToken(token);
     }
 
@@ -28,15 +42,19 @@ export class UsersService {
     if (isVerified) {
       const { email, fullName } = loginUserDto;
 
-      console.log(`emai: ${email}`)
-      console.log(`fullName: ${fullName}`)
+      console.log(`emai: ${email}`);
+      console.log(`fullName: ${fullName}`);
 
-      await this.userModel.findOneAndUpdate({ email: email }, { $set: loginUserDto }, { upsert: true, new: true });
-      accessToken = this.jwtService.sign(loginUserDto)
+      await this.userModel.findOneAndUpdate(
+        { email: email },
+        { $set: loginUserDto },
+        { upsert: true, new: true }
+      );
+      accessToken = this.jwtService.sign(loginUserDto);
     }
 
     return {
-      access_token: accessToken
+      access_token: accessToken,
     };
   }
 }
